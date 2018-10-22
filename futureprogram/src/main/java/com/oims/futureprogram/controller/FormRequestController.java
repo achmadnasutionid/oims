48a7af05d477d20fc10ed874019c0eff20c6c549
Achmad Nasution <achmadynasution@gmail.com>
2018-10-22 14:40:10 +0700

    Menyelesaikan CRUD Form Request

--- futureprogram/src/main/java/com/oims/futureprogram/controller/FormRequestController.java
+++ futureprogram/src/main/java/com/oims/futureprogram/controller/FormRequestController.java
@@ -7,6 +7,7 @@ import com.oims.futureprogram.repository.FormRequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -38,9 +39,35 @@ public class FormRequestController {
     }
 
     @PostMapping("/employee/{employeeId}/formrequest")
-    public FormRequest createFormRequest(@Valid @RequestBody FormRequest formrequest) {
-        return formrequestRepository.save(formrequest);
+    public FormRequest createFormRequest(@Valid @RequestBody FormRequest formrequest, @PathVariable Long employeeId) {
+        return employeeRepository.findById(employeeId).map(employee -> {
+            formrequest.setEmployee(employee);
+            return formrequestRepository.save(formrequest);
+        }).orElseThrow(() -> new ResourceNotFoundException("Employee not found with Id "+employeeId));
     }
 
+    @PutMapping("/employee/{employeeId}/formrequest/{formrequestId]")
+    public FormRequest updateFormRequest(@PathVariable Long employeeId, @PathVariable Long formrequestId, @Valid @RequestBody FormRequest formRequestrequest) {
+        if(!employeeRepository.existsById(employeeId)) {
+            throw new ResourceNotFoundException("Employee not found with Id " + employeeId);
+        }
+        return formrequestRepository.findById(formrequestId).map(formRequest -> {
+            formRequest.setNama(formRequestrequest.getNama());
+            formRequest.setJumlah(formRequestrequest.getJumlah());
+            formRequest.setTanggal(formRequestrequest.getTanggal());
+            formRequest.setStatus(formRequestrequest.getStatus());
+            return formrequestRepository.save(formRequest);
+        }).orElseThrow(() -> new ResourceNotFoundException("Form Request not found with Id " + formrequestId));
+    }
 
+    @DeleteMapping("/employee/{employeeId}/formrequest/{formrequestId}")
+    public ResponseEntity<?> deleteTodo(@PathVariable Long employeeId, @PathVariable Long formrequestId) {
+        if(!employeeRepository.existsById(employeeId)) {
+            throw new ResourceNotFoundException("Employee not found with Id "+ employeeId);
+        }
+        return formrequestRepository.findById(formrequestId).map(formRequest -> {
+            formrequestRepository.delete(formRequest);
+            return ResponseEntity.ok().build();
+        }).orElseThrow(() -> new ResourceNotFoundException("Form Request not found with Id" + formrequestId));
+    }
 }
